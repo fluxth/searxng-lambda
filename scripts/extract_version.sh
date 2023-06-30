0e6545c8122aafa9f7f1aa13de4ceda438f888b6
Thitat Auareesuksakul <thitat@flux.ci>
2023-06-30 23:21:03 +0900

    ci: clense old release scheme and switch to semver

--- scripts/extract_version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-CWD="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-
-RUNTIME_TAG=$(grep lambda/python $CWD/../Dockerfile | head -n1 | sed 's/@.*$//' | sed 's/^.*://')
-SEARXNG_TAG=$(grep DOCKER_TAG $CWD/../searxng/searx/version_frozen.py | sed 's/^.*= "//' | sed 's/"$//')
-
-echo "$SEARXNG_TAG-$RUNTIME_TAG"
